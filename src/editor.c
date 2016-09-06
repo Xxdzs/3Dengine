c8f8a9e7a63407c0f731331affde02c8bcca75dc
Xxdzs <sid.xxdzs@gmail.com>
2016-09-06 09:41:46 +0200

    Much better

--- src/editor.c
+++ src/editor.c
@@ -6,38 +6,49 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/06 05:09:30 by angagnie          #+#    #+#             */
-/*   Updated: 2016/09/06 08:31:30 by angagnie         ###   ########.fr       */
+/*   Updated: 2016/09/06 09:39:16 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ggl.h"
 
 
-
-int		editor_repaint(t_env *w)
+void	local_render(t_env *w, t_pnt2i o, t_pnt2i d, t_fnray f)
 {
-	t_pnt2i p;
-	t_pnt2i d;
+	t_pnt2i		p;
+	unsigned	ans;
 
-	d = (t_pnt2i){w->data.tile, w->data.tile};
 	p.y = d.y;
 	while (p.y-- > 0)
 	{
 		p.x = d.x;
 		while (p.x-- > 0)
-			pxl_on(w, d.x + p.x, d.y + p.y,
-			frac_render(w, p, d));
+			if ((ans = f(w, p, d)))
+				pxl_on(w, o.x + p.x, o.y + p.y, ans);
 	}
-	size_t n = 2;
-	p.x = d.x >> n;
-	while (p.x > 0)
+}
+
+void	place_tile(t_env *w, t_pnt2i o, t_fnray f)
+{
+	const size_t	c = w->data.tile;
+	const size_t	d = (3 * c) / 2;
+
+	w->frc.zoom *= 0.95;
+	w->frc.is_julia ^= 1;
+	local_render(w, (t_pnt2i){o.x * d + c / 2, o.y * d + c / 2},
+				 (t_pnt2i){c, c}, f);
+}
+
+int		editor_repaint(t_env *w)
+{
+	t_pnt2i o;
+
+	o.y = 4;
+	while (o.y-- > 0)
 	{
-		p.y = d.y + (p.x << 1);
-		while (p.y-- > 0)
-			pxl_on(w, 2*d.x + p.x, d.y + p.y - p.x,
-				frac_render(w, (t_pnt2i){p.x << n, p.y},
-					(t_pnt2i){d.x, d.y + (p.x << 1)}));
-		p.x--;
+		o.x = 7;
+		while (o.x-- > 0)
+			place_tile(w, o, &frac_render);
 	}
 	return (0);
 }
--- src/wolf_main.c
+++ src/wolf_main.c
@@ -6,7 +6,7 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/30 19:15:59 by angagnie          #+#    #+#             */
-/*   Updated: 2016/09/06 08:20:10 by angagnie         ###   ########.fr       */
+/*   Updated: 2016/09/06 09:41:07 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,17 @@ int		editor_submain(t_env *w, int ac, char **av)
 {
 	ft_putstr("Editor Sub-main\n");
 	frac_submain(w, ac, av);
-	w->frc.is_julia = 1;
+	w->frc.is_julia = 0;
 	w->frc.is_locked = 1;
-	w->frc.zoom = 3;
+	w->frc.zoom = 4;
 	w->frc.max_iter = 500;
+	w->frc.cntr = NEW_CMPLX(-1, 0);
+	w->frc.c = NEW_CMPLX(-1, 0);
 	w->fnct.repaint = &editor_repaint;
 	w->fnct.key_hook = &editor_key_hook;
 	w->fnct.mouse_click = &editor_mouse_click;
 	w->fnct.mouse_move = &editor_mouse_move;
-	w->data.tile = 512;
+	w->data.tile = 128;
 	return (0);
 }
 
